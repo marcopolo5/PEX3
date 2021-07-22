c20cc44a613af7c5246116b048cd7a7b57a80775
DAVID <DAVID@DESKTOP-D6LHSK1>
2021-07-22 23:22:39 +0300

    Change WPF project from .net framework to .net 5. Added dependency injection in App.xaml.cs

---
+++ UI.WPF/App.xaml.cs
@@ -0,0 +1,54 @@
+﻿using AccountModule.Controllers;
+using Domain.AccountContracts;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+namespace MainWindoww
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        // Dependency Injection Provider:
+        private readonly ServiceProvider _serviceProvider;
+
+        public App()
+        {
+            // setting up the dependency injection:
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+        }
+
+        /// <summary>
+        /// You can add new dependencies in this method
+        /// </summary>
+        /// <param name="services">Use this argument to add the dependencies</param>
+        private void ConfigureServices(IServiceCollection services)
+        {
+            // Adding the main window as a singleton
+            services.AddSingleton<MainWindow>();
+
+            // Adding  ApplicationUserController as scoped
+            services.AddScoped<IAccountService, ApplicationUserController>();
+        }
+
+
+        /// <summary>
+        /// This is executed when the app is started.
+        /// This method receives the MainWindow object from the dependency container and displays it
+        /// </summary>
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            var mainWindow = _serviceProvider.GetService<MainWindow>();
+            mainWindow.Show();
+        }
+
+    }
+}
