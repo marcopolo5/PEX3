dc159c43166ecadd9ccda42b060eb6ed5539464a
DAVID <DAVID@DESKTOP-D6LHSK1>
2021-07-22 22:46:23 +0300

    Added conversation field back to Message class to fix a bug

--- Domain/Models/Message.cs
+++ Domain/Models/Message.cs
@@ -11,7 +11,8 @@ namespace Domain.Models
 
         [Description("ignore")]
         public User Sender { get; set; }
-        
+
+        public Conversation Conversation { get; set; }
         public DateTime CreatedAt { get; set; }
         public string TextMessage { get; set; }
     }
