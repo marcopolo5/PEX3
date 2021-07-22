f36670e8c54f31388185654034022e4428db2fa8
DAVID <DAVID@DESKTOP-D6LHSK1>
2021-07-22 22:47:06 +0300

    Added user field back to Profile class to fix a bug.

--- Domain/Models/Profile.cs
+++ Domain/Models/Profile.cs
@@ -7,6 +7,7 @@ namespace Domain.Models
     public class Profile
     {
         public int Id { get; set; }
+        public User User { get; set; }
         public string DisplayName { get; set; }
         public UserStatus Status { get; set; }
     }
