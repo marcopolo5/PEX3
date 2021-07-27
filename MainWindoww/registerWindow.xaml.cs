9f6bbd7e406302b7e99e9a84d9fdbd1207fc51d6
Sorina Ioja <sorina_ioja@yahoo.com>
2021-07-27 20:33:57 +0300

    round border main window and register

--- MainWindoww/registerWindow.xaml.cs
+++ MainWindoww/registerWindow.xaml.cs
@@ -33,15 +33,27 @@ namespace MainWindoww
             password2Text.Clear();
         }
 
-        private void returnButton_Click(object sender, RoutedEventArgs e)
+        private void loginTextBox_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
+            loginWindow login = new loginWindow();
+            login.Show();
             this.Hide();
-            //mainWindow.Show()
         }
 
-        private void loginButton_Click(object sender, RoutedEventArgs e)
+        private void returnTextBox_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
-
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Hide();
+        }
+        private void grid_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+      
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
