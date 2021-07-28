f55e3fd27466f1fa86ab6fa702bcb4fcaa2f89bf
DAVID <DAVID@DESKTOP-D6LHSK1>
2021-07-28 15:27:24 +0300

    Added ITokenFileSaver interface

---
+++ Domain/AccountContracts/ITokenFileSaver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.AccountContracts
+{
+    public interface ITokenFileSaver
+    {
+        bool SaveToken(string token);
+        bool DeleteToken();
+        string GetToken(string token);
+    }
+}
