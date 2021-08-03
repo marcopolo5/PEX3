72322653d86c1294d83f68669eece49e6d430996
DAVID <DAVID@DESKTOP-D6LHSK1>
2021-08-03 11:38:49 +0300

    Added cors and mapped signalR ChatHub to /chat endpoint

--- ChatServerModule/Startup.cs
+++ ChatServerModule/Startup.cs
@@ -1,3 +1,4 @@
+using ChatServerModule.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +17,8 @@ namespace ChatServerModule
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
+            services.AddSignalR(options => options.MaximumReceiveMessageSize = 102400000);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,13 +30,16 @@ namespace ChatServerModule
             }
 
             app.UseRouting();
+            app.UseCors(builder => builder
+                //.AllowAnyOrigin()
+                .WithOrigins("null")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials());
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapHub<ChatHub>("/chat");
             });
         }
     }
