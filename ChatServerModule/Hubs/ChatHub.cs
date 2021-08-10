dec62085ea01577fe6fd4b8d01e8c8c8fea92745
DAVID <DAVID@DESKTOP-D6LHSK1>
2021-08-10 16:05:03 +0300

    ServerSide: Added UpdateStatus() to notify when a user is online/offline

--- ChatServerModule/Hubs/ChatHub.cs
+++ ChatServerModule/Hubs/ChatHub.cs
@@ -15,20 +15,28 @@ namespace ChatServerModule.Hubs
     {
 
         /// <summary>
-        /// key - user id
+        /// key - user id 
         /// value - connection
         /// </summary>
         public static readonly ConcurrentDictionary<int, string> ConnectedUsers = new();
 
-
         private readonly ITokenValidator _tokenValidator;
         private readonly IConversationRepo _conversationRepo;
-        public ChatHub(ITokenValidator tokenValidator, IConversationRepo conversationRepo)
+        private readonly IUsersRepo _usersRepo;
+
+        public ChatHub(ITokenValidator tokenValidator, 
+            IConversationRepo conversationRepo,
+            IUsersRepo usersRepo)
         {
             _tokenValidator = tokenValidator;
             _conversationRepo = conversationRepo;
+            _usersRepo = usersRepo;
         }
 
+        /// <summary>
+        /// Called when user connects to the server
+        /// </summary>
+        /// <returns>A task</returns>
         public override async Task OnConnectedAsync()
         {
             string stringId = Context
@@ -51,19 +59,38 @@ namespace ChatServerModule.Hubs
                 return;
             }
 
-           ConnectedUsers[id] = Context.ConnectionId;
+            ConnectedUsers[id] = Context.ConnectionId;
+
+            // update status for friends
+            await UpdateStatus(id, UserStatus.Online);
+
             await base.OnConnectedAsync();
         }
 
-        public override Task OnDisconnectedAsync(Exception exception)
+        /// <summary>
+        /// Called when user disconnects from the server
+        /// </summary>
+        /// <param name="exception">An exception</param>
+        /// <returns>A task</returns>
+        public override async Task OnDisconnectedAsync(Exception exception)
         {
             // get the userId
             var userId = ConnectedUsers.FirstOrDefault(cd => cd.Value == Context.ConnectionId).Key;
+
+            // update status for friends
+            await UpdateStatus(userId, UserStatus.Offline);
+
             // remove user from the connected user lists
             ConnectedUsers.Remove(userId, out string _); //discarding the out param
-            return base.OnDisconnectedAsync(exception);
+            await OnDisconnectedAsync(exception);
         }
 
+
+        /// <summary>
+        /// This method can be called from client to send a message
+        /// </summary>
+        /// <param name="message"> Message to be sent</param>
+        /// <returns>A task</returns>
         public async Task SendMessage(Message message)
         {
             IEnumerable<int> userIds = _conversationRepo.GetUserIds(message.ConversationId);
@@ -81,5 +108,50 @@ namespace ChatServerModule.Hubs
             }
         }
 
+
+        /// <summary>
+        /// Get connection ids from all the user's friends(if they are connected) 
+        /// </summary>
+        /// <param name="currentUser"></param>
+        /// <returns>Returns a list of connection ids</returns>
+        private List<string> GetFriendsConnectionIds(int currentUser)
+        {
+            var friendsIds = _usersRepo.GetFriendsIds(currentUser);
+            var result = new List<string>();
+            foreach (var id in friendsIds)
+            {
+                if (ConnectedUsers.TryGetValue(id, out string connectionId))
+                {
+                    result.Add(connectionId);
+                }
+            }
+            return result;
+        }
+
+
+        /// <summary>
+        /// Update this user status for all connected friends
+        /// </summary>
+        /// <param name="userId">User ID</param>
+        /// <param name="newStatus">New status</param>
+        /// <returns>A task</returns>
+        private async Task UpdateStatus(int userId, UserStatus newStatus)
+        {            
+            // create a status model
+            var statusModel = new StatusModel
+            {
+                FriendId = userId,
+                NewStatus = newStatus
+            };
+
+            // get all online friends
+            IReadOnlyList<string> allFriendsConnection = GetFriendsConnectionIds(userId);
+
+            // if there's at least one friend connected call ChangeStatus on client
+            if (allFriendsConnection.Count > 0)
+            {
+                await Clients.Clients(allFriendsConnection).SendAsync("ChangeStatus", statusModel);
+            }
+        }
     }
 }
---
+++ ChatServerModule/MiniRepo/IUsersRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatServerModule.MiniRepo
+{
+    public interface IUsersRepo
+    {
+        /// <summary>
+        /// Get all ids from friends
+        /// </summary>
+        /// <param name="userId">User id</param>
+        /// <returns>A list of friends' ids</returns>
+        IEnumerable<int> GetFriendsIds(int userId);
+    }
+}
