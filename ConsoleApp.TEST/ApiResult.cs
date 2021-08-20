0be2704e116757dd2cced4628a7b2358b362786c
DAVID <DAVID@DESKTOP-D6LHSK1>
2021-08-20 22:45:10 +0300

    Moved ApiResult to a separated file

---
+++ ConsoleApp.TEST/ApiResult.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.TEST
+{
+    /// <summary>
+    /// A simple model that encapsulates the data returned by the external API.
+    /// </summary>
+    public class ApiResult
+    {
+        public string Ip { get; set; }
+
+        [JsonProperty(PropertyName = "continent_name")]
+        public string ContinentName { get; set; }
+
+        [JsonProperty(PropertyName = "country_name")]
+        public string CountryName { get; set; }
+
+        [JsonProperty(PropertyName = "region_name")]
+        public string RegionName { get; set; }
+
+        public string City { get; set; }
+
+        public string Zip { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public override string ToString()
+        {
+            return $"Ip: {Ip} | Continent: {ContinentName} | Country: {CountryName} | Region: {RegionName} | City: {City} | Coords: {Latitude} : {Longitude}";
+        }
+    }
+}
--- ConsoleApp.TEST/Program.cs
+++ ConsoleApp.TEST/Program.cs
@@ -11,26 +11,6 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp.TEST
 {
-    public class ApiResult
-    {
-        public string Ip { get; set; }
-        [JsonProperty(PropertyName = "continent_name")]
-        public string ContinentName { get; set; }
-        [JsonProperty(PropertyName = "country_name")]
-        public string CountryName { get; set; }
-        [JsonProperty(PropertyName = "region_name")]
-        public string RegionName { get; set; }
-        public string City { get; set; }
-        public string Zip { get; set; }
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
-
-        public override string ToString()
-        {
-            return $"Ip: {Ip} | Continent: {ContinentName} | Country: {CountryName} | Region: {RegionName} | City: {City} | Coords: {Latitude} : {Longitude}";
-        }
-
-    }
     public class Program
     {
 
