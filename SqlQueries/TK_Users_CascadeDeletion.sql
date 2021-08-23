f5b44a938a361370d5859e78ec1207d9769f0946
Rares Butean <59267118+buteanrares@users.noreply.github.com>
2021-08-23 17:55:39 +0300

    Cascade bugfix. Added deletion trigger. Updated tables and constraints.

---
+++ SqlQueries/TK_Users_CascadeDeletion.sql
@@ -0,0 +1,19 @@
+create or alter trigger TK_Users_CascadeDeletion
+	on Users
+	instead of delete
+as
+begin
+	set nocount on;
+	delete from Blocked_Users where (userid in (select id from deleted)
+		or blockeduserid in (select id from deleted))
+	delete from Friend_Requests where (senderid in (select id from deleted)
+		or receiverid in (select id from deleted))
+	delete from Group_Members where userid in (select id from deleted)
+	delete from Messages where senderid in (select id from deleted)
+	delete from Profiles where userid in (select id from deleted)
+	delete from Settings where userid in (select id from deleted)
+	delete from Strikes where userid in (select id from deleted)
+	delete from UserReports where (userid in (select id from deleted)
+		or reporteduserid in (select id from deleted))
+	delete from Users where id in (select id from deleted)
+end
