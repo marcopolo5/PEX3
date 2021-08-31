6f5cccfcec1d27ab1ac037d40111497495897ca9
DAVID <DAVID@DESKTOP-D6LHSK1>
2021-08-31 23:06:53 +0300

    added earth's radius to a variable

--- ChatServerModule/Hubs/ChatHub.cs
+++ ChatServerModule/Hubs/ChatHub.cs
@@ -169,7 +169,7 @@ namespace ChatServerModule.Hubs
                 var distance = DistanceCalculator.CalculateDistance(locationDTO.Latitude, locationDTO.Longitude,
                                                                     conversation.Latitude, conversation.Longitude);
                 Console.WriteLine($"{conversation.Location} | {conversation.Latitude} | {conversation.Longitude}");
-                // calculate distance in km and check if its higher than userProximityRadius, if so go to the next conversation
+                // calculate distance in km and check if it s higher than userProximityRadius, if so go to the next conversation
                 if (distance / 1000 > userProximityRadius)
                 {
                     continue;
--- ChatServerModule/ProximityChatLogic/DistanceCalculator.cs
+++ ChatServerModule/ProximityChatLogic/DistanceCalculator.cs
@@ -18,13 +18,14 @@ namespace ChatServerModule.ProximityChatLogic
         /// <returns>The distance in meters</returns>
         public static double CalculateDistance(double sLatitude, double sLongitude, double eLatitude, double eLongitude)
         {
+            var earthRadius = 6376500.0;
             var d1 = sLatitude * (Math.PI / 180.0);
             var num1 = sLongitude * (Math.PI / 180.0);
             var d2 = eLatitude * (Math.PI / 180.0);
             var num2 = eLongitude * (Math.PI / 180.0) - num1;
             var d3 = Math.Pow(Math.Sin((d2 - d1) / 2.0), 2.0) + Math.Cos(d1) * Math.Cos(d2) * Math.Pow(Math.Sin(num2 / 2.0), 2.0);
 
-            return 6376500.0 * (2.0 * Math.Atan2(Math.Sqrt(d3), Math.Sqrt(1.0 - d3)));
+            return earthRadius * (2.0 * Math.Atan2(Math.Sqrt(d3), Math.Sqrt(1.0 - d3)));
         }
     }
 }
