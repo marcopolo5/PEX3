947aac51d9dee3f3365627abae6dcace13dab972
Sorina Ioja <sorina_ioja@yahoo.com>
2021-09-07 12:45:42 +0300

    setting button style

--- ChatModule/SignalRClient.cs
+++ ChatModule/SignalRClient.cs
@@ -54,8 +54,8 @@ namespace ChatModule
 
         public async Task InitializeConnectionAsync(int userId, string token)
         {
-            var url = @"http://localhost:5000/chat";
-
+            //var url = @"http://localhost:5000/chat";
+            var url = @"http://79.113.39.95:5000/chat";
             _connection = new HubConnectionBuilder()
                 .WithUrl(url, options =>
                 {
